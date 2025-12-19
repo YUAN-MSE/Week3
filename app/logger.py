bf9eab247a5fd416d90abd16fbc6e895b1264221
YUAN-MSE <rp2txsgmsn@privaterelay.appleid.com>
2025-12-19 11:42:56 +0000

    Rename dir

--- app/logger.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-
-def setup_logging(level: str) -> logging.Logger:
-    logging.basicConfig(
-        level=level,
-        format="%(asctime)s %(levelname)s %(name)s - %(message)s",
-    )
-    return logging.getLogger("clinic")
